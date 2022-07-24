1cfa8caeab87bf9dba250d5e42d5037cf19e2150
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2022-07-24 22:10:49 +0200

    [CORE] - Create object for libraries and includes Hilt dependencies

--- buildSrc/src/main/java/com/jmonzon/pokeapi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/jmonzon/pokeapi/buildsrc/dependencies.kt
@@ -1,2 +1,11 @@
 package com.jmonzon.pokeapi.buildsrc
 
+object Libs {
+    object Hilt {
+        private const val version = "2.43"
+        const val android = "com.google.dagger:hilt-android:$version"
+        const val compiler = "com.google.dagger:hilt-compiler:$version"
+        const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val test = "com.google.dagger:hilt-android-testing:$version"
+    }
+}
