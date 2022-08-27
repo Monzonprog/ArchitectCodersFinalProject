fee15b3f97bd69fb9b77ed61990e81579b9b8ad8
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2022-08-27 15:20:14 +0200

    [WIP] - includes room library

--- buildSrc/src/main/java/com/jmonzon/pokeapi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/jmonzon/pokeapi/buildsrc/dependencies.kt
@@ -80,4 +80,11 @@ object Libs {
     object JavaX {
         const val inject = "javax.inject:javax.inject:1"
     }
+
+    object Room {
+        private const val version = "2.4.3"
+        const val runtime = "androidx.room:room-runtime:$version"
+        const val ktx = "androidx.room:room-ktx:$version"
+        const val compiler = "androidx.room:room-compiler:$version"
+    }
 }
