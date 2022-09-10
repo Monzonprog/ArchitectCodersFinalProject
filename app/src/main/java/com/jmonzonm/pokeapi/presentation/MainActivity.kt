1b413851b7151e4fb72434c8067e98c901aabc7f
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2022-09-10 14:00:39 +0200

    [WIP] - fix problems with system ui bars using themes

--- app/src/main/java/com/jmonzonm/pokeapi/presentation/MainActivity.kt
+++ app/src/main/java/com/jmonzonm/pokeapi/presentation/MainActivity.kt
@@ -6,7 +6,6 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.jmonzonm.pokeapi.R
 import com.jmonzonm.pokeapi.presentation.dialog.CustomsDialogs
@@ -25,8 +24,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-
         initSensor()
     }
 
